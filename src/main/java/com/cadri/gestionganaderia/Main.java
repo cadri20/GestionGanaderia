746762aef0deb27fe8bcc5c11fede221859d60ab
Adrian <cadrian_2001@hotmail.com>
2021-03-30 15:01:51 -0500

    Interfaces iniciales

---
+++ src/main/java/com/cadri/gestionganaderia/Main.java
@@ -0,0 +1,31 @@
+package com.cadri.gestionganaderia;
+
+import gui.Inicio;
+import gui.MenuPrincipal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author cadri
+ */
+public class Main {
+    public static void main(String[] args){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            JFrame inicio = new Inicio();
+            inicio.setVisible(true);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
